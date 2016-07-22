015851aa8184393d0282deaa898bbd4adc013ace
KalilaCalame <kc3341@nyu.edu>
2016-07-22 11:21:33 -0400

    Add files via upload

---
+++ crypted.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  8 13:39:23 2016
+
+@author: student
+"""
+
+def oneTimePad(text,pad):
+    covertedText = ""
+    i = 8=0
+    for letter in text:
+        convertedText
